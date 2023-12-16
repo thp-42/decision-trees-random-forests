ef28ecb50e2d81735bc1e38d56a77425c8f22d22
Tanish Panjwani <tanishhasanali@gmail.com>
2023-12-16 14:58:58 +0300

    practise with data set

--- decision-trees-random-forests.R
+++ decision-trees-random-forests.R
@@ -1,8 +1,10 @@
 #install.packages('rpart')
 #install.packages('rpart.plot')
+#install.packages('randomForest')
 
 library(rpart)
 library(rpart.plot)
+library(randomForest)
 
 str(kyphosis)
 head(kyphosis)
@@ -12,3 +14,6 @@ plot(tree, uniform = TRUE, main = 'Kyphosis Tree')
 printcp(tree)
 text(tree, use.n = TRUE, all=TRUE)
 prp(tree)
+
+rf.model <- randomForest(Kyphosis ~ ., data = kyphosis)
+print(rf.model)
